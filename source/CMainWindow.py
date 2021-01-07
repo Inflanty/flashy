d5b7827d31333653a743379288a451239f6820f8
Inflanty <inflanty@gmail.com>
2021-01-07 15:47:25 +0100

    Add graphic widget

--- source/CMainWindow.py
+++ source/CMainWindow.py
@@ -14,12 +14,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1007, 676)
+        MainWindow.resize(925, 587)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        self.graphicWidget = QtWidgets.QWidget(self.centralwidget)
+        self.graphicWidget.setGeometry(QtCore.QRect(79, 49, 731, 451))
+        self.graphicWidget.setObjectName("graphicWidget")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1007, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 925, 21))
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
